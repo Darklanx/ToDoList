113c959f6e79c3f72ff8f36f2e7783fe7293ac99
Rabitomaster de Medici <littlecrazymouse.cs06@nctu.edu.tw>
2018-04-26 15:36:52 +0800

    no message

--- dbtest.js
+++ dbtest.js
@@ -1,11 +1,24 @@
 var local_db = new PouchDB('ToDos');
-var remoteDB = new PouchDB('http://localhost:5984/myremotedb')
-remoteDB.info().then(function (info) {
-});
+var remoteDB = new PouchDB('http://localhost:5984/todos')
 
-// local_db.sync(remoteDB).on("complete",function(){
-// 	console.log("success Sync");
-// }).on("error",function(err){
-// 	console.log(err);
-// });
+var default_todos = {
+	"_id" : "",
+	"date" : -1,
+	"context" : "",
+	"alarm" : false,
+	
+
+}
+
+function add(){
+
+}
+
+function remove(){
+
+}
+
+function edit(){
+
+}
 
